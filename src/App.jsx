0bb2cb6d735c6be9ed37d0d3233432122508ae1c
sean-bamford <97364673+sean-bamford@users.noreply.github.com>
2024-02-26 21:48:49 +0000

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,19 +24,19 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Sean Bamford",
+  title: "Coder & Meatball",
+  email: "sean.bamford.sb@gmail.com",
+  gitHub: "sean-bamford",
+  instagram: "sean.bamford",
+  linkedIn: "sean-bamford",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
-const primaryColor = "#4E567E";
-const secondaryColor = "#D2F1E4";
+const primaryColor = "#1579A8";
+const secondaryColor = "#2D5669";
 
 const App = () => {
   return (
